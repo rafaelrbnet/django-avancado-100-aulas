d8e4121298da4ee948cb1ab5926e1293a68c896c
Rafael Baena Neto <rafael.rbnet@gmail.com>
2019-10-29 22:00:56 -0300

    Substituição das funções para Django Class Based Views

--- clientes/views.py
+++ clientes/views.py
@@ -1,47 +1,47 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from django.urls import reverse_lazy
 from .models import Person
-from .forms import PersonForm
 from django.views.generic.list import ListView
-
-
-@login_required
-def persons_new(request):
-    form = PersonForm(request.POST or None, request.FILES or None)
-
-    if form.is_valid():
-        form.save()
-        return redirect('person_list')
-    return render(request, 'person_form.html', {'form': form})
-
-
-@login_required
-def persons_update(request, id):
-    person = get_object_or_404(Person, pk=id)
-    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
-
-    if form.is_valid():
-        form.save()
-        return redirect('person_list')
-
-    return render(request, 'person_form.html', {'form': form})
-
-
-@login_required
-def persons_delete(request, id):
-    person = get_object_or_404(Person, pk=id)
-
-    if request.method == 'POST':
-        person.delete()
-        return redirect('person_list')
-
-    return render(request, 'person_delete_confirm.html', {'person': person})
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 @method_decorator(login_required, name='dispatch')
 class PersonList(ListView):
     model = Person
-    paginate_by = 1
+    paginate_by = 10
 
+
+@method_decorator(login_required, name='dispatch')
+class PersonDetail(DetailView):
+    model = Person
+
+
+@method_decorator(login_required, name='dispatch')
+class PersonCreate(SuccessMessageMixin, CreateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list')
+    success_message = "%(first_name)s cadastrado com sucesso"
+
+
+@method_decorator(login_required, name='dispatch')
+class PersonUpdate(SuccessMessageMixin, UpdateView):
+    model = Person
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo']
+    success_url = reverse_lazy('person_list')
+    success_message = "%(first_name)s atualizado com sucesso"
+
+
+@method_decorator(login_required, name='dispatch')
+class PersonDelete(SuccessMessageMixin, DeleteView):
+    model = Person
+    success_url = reverse_lazy('person_list')
+    success_message = "Registro excluído com sucesso"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(PersonDelete, self).delete(request, *args, **kwargs)
--- readme.md
+++ readme.md
@@ -15,3 +15,30 @@ Aula 8
 Class Based Views CBD  
 Documentação do Django: https://docs.djangoproject.com/en/2.2/ref/class-based-views/
 
+Aula 9  
+ListViews: O django porcura automaticamente o nome dos templates de cada método.   
+Ex: Classe: `class PersonList(ListView):` Model: `model = Person` o nome do template automaticamente é declarado e deve ser salvo em `clientes/templates/clientes/pperson_list.html`  
+Paginação: Na view deve ser declarada `paginate_by = 1`  
+No template:
+```
+{% if is_paginated %}
+    <div class="pagination">
+        <span class="page-links">
+            {% if page_obj.has_previous %}
+                <a href="/clientes/list?page={{ page_obj.previous_page_number }}">anterior</a>
+            {% endif %}
+            <span class="page-current">
+                Página {{ page_obj.number }} de {{ page_obj.paginator.num_pages }}.
+            </span>
+            {% if page_obj.has_next %}
+                <a href="/clientes/list?page={{ page_obj.next_page_number }}">próximo</a>
+            {% endif %}
+        </span>
+    </div>
+{% endif %}
+```
+            
+ Aula 10  
+ DetailViews:Mesmo padrão de nomenclatura de templates
+ 
+ 
